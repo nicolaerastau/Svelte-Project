bfeef026ee2a59b018a1ef1e964ba67ba2c25e99
nicolaerastau <nickqsr@yahoo.com>
2020-02-23 21:10:41 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,143 @@
+<script>
+  import Product from "./Product.svelte";
+  import Button from "./Button.svelte";
+  import Cart from "./Cart.svelte";
+
+  let title = "";
+  let price = "";
+  let description = "";
+
+  let products = [];
+  let cartItems = [];
+
+  function setTitle(event) {
+    title = event.target.value;
+  }
+
+  function createProduct() {
+    const newProduct = {
+      title: title,
+      price: price,
+      description: description
+    };
+
+    products = [...products, newProduct];
+    //products = products.concat(newProduct);
+  }
+
+  function removeProduct(e) {
+    //const title = e.detail;
+    products = products.filter(prod => prod.title !== e.detail);
+  }
+
+  function addToCart(event) {
+    const selectedTitle = event.detail;
+    cartItems = cartItems.concat({
+      ...products.find(prod => prod.title === selectedTitle)
+    });
+  }
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    background-color: rgb(67, 125, 151);
+    padding: 10px;
+    display: block;
+    margin: 0 auto;
+    border-radius: 5px;
+  }
+  @media (min-width: 768px) {
+    .container {
+      width: 35rem;
+    }
+  }
+
+  .section {
+    width: 22rem;
+    height: auto;
+    margin: auto;
+  }
+  @media (max-width: 767px) {
+    .section {
+      width: 17rem;
+      padding: 10px;
+      margin: auto;
+    }
+  }
+
+  label {
+    background-color: rgb(6, 166, 194);
+    margin-bottom: 0.5rem;
+    margin-left: 0;
+    width: 40%;
+    height: 1.5rem;
+    color: #fff;
+    text-align: center;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  input,
+  textarea {
+    width: 100%;
+    border-radius: 4px;
+    color: #333;
+  }
+  p {
+    color: #fff;
+  }
+  .title {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  @media (max-width: 465px) {
+    .title,
+    .price,
+    .description {
+      width: 14rem;
+    }
+  }
+</style>
+
+<section>
+  <Cart items={cartItems} />
+</section>
+
+<hr />
+<div class="container">
+  <section class="section">
+
+    <div class="title">
+      <label for="title">Title</label>
+      <input type="text" id="title" value={title} on:input={setTitle} />
+    </div>
+    <div class="price">
+      <label for="price">Price</label>
+      <input type="number" id="price" bind:value={price} />
+    </div>
+    <div class="description">
+      <label for="description">Description</label>
+      <textarea rows="3" bind:value={description} />
+    </div>
+
+    <Button buttonPrimary on:click={createProduct}>Create Product</Button>
+  </section>
+
+  <section>
+    {#if products.length === 0}
+      <p>No products were added yet!</p>
+    {:else}
+      {#each products as product}
+        <Product
+          productTitle={product.title}
+          productPrice={product.price}
+          productDescription={product.description}
+          on:addcart={addToCart}
+          on:removeproduct={removeProduct} />
+      {/each}
+    {/if}
+  </section>
+</div>
---
+++ src/Button.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let buttonSecondary = false;
+  export let buttonPrimary = false;
+</script>
+
+<style>
+  .buttonPrimary {
+    font: inherit;
+    padding: 0.15rem 0.5rem;
+    background: #d10057;
+    border: 0.5px solid rgb(6, 166, 194);
+    color: white;
+    cursor: pointer;
+    margin-top: 10px;
+  }
+  .buttonSecondary {
+    background: rgb(6, 166, 194);
+    font: inherit;
+    padding: 0.15rem 0.5rem;
+    border: 0.5px solid #d10057;
+    color: white;
+    cursor: pointer;
+  }
+
+  @media (max-width: 465px) {
+    .buttonSecondary {
+      margin-top: 10px;
+    }
+  }
+
+  button:hover:hover,
+  button:active {
+    box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
+  }
+</style>
+
+<button class:buttonSecondary class:buttonPrimary on:click>
+  <slot />
+</button>
